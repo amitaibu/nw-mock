ccbf8fae95dace4cf423adef2a08056d41d3e412
Amitai Burstein <amitai@gizra.com>
2016-11-08 00:18:06 +0200

    Wire cards

--- src/elm/Pages/Notifications/View.elm
+++ src/elm/Pages/Notifications/View.elm
@@ -1,13 +1,20 @@
 module Pages.Notifications.View exposing (view)
 
+import Dict exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import Notification.View exposing (..)
 import Pages.Notifications.Model exposing (..)
 import Pages.Notifications.Update exposing (..)
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        []
+    let
+        viewNotification ( id, notification ) =
+            Notification.View.view notification
+    in
+        div
+            []
+            (List.map viewNotification <| Dict.toList model.notifications)
